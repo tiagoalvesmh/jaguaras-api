b94282daa794ae254929a2c55ae43973b796a034
Tiago Mohnschmidt <tiagoalvesmh@gmail.com>
2020-08-05 12:03:11 -0300

    Rotas de CRUD criadas

--- start/routes.js
+++ start/routes.js
@@ -1,20 +1,23 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
 Route.post('/users', 'UserController.create')
 Route.post('/sessions', 'SessionController.create')
+
+Route.group(() => {
+  Route.resource('cities', 'CityController')
+    .apiOnly()
+  
+  Route.resource('gyms', 'GymController')
+    .apiOnly()
+
+  Route.resource('courts', 'CourtController')
+    .apiOnly()
+
+  Route.resource('matches', 'MatchController')
+    .apiOnly()
+  
+  Route.resource('clubs', 'ClubController')
+    .apiOnly()
+}).middleware('auth')
