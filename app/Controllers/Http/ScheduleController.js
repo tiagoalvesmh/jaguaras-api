3d0f160a7ab71d39c0906fede925ea4c1adc8659
Tiago Mohnschmidt <tiagoalvesmh@gmail.com>
2020-08-06 17:19:10 -0300

    CRUD de horários implementado

--- app/Controllers/Http/ScheduleController.js
+++ app/Controllers/Http/ScheduleController.js
@@ -1,31 +1,49 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Schedule = use('App/Models/Schedule')
 
-/**
- * Resourceful controller for interacting with schedules
- */
 class ScheduleController {
   
   async index ({ request, response, view }) {
+    const schedule = Schedule.all()
+
+    return schedule
   }
 
 
   async store ({ request, response }) {
+    const data = request.only(["match_id", "date", "start_time", "end_time", "fixed", "available"])
+
+    const schedule = await Schedule.create(data)
+
+    return schedule
   }
 
 
   async show ({ params, request, response, view }) {
+    const schedule = await Schedule.findOrFail(params.id)
+
+    return schedule
   }
 
 
   async update ({ params, request, response }) {
+    const schedule = await Schedule.findOrFail(params.id)
+
+    const data = request.only(["match_id", "date", "start_time", "end_time", "fixed", "available"])
+
+    schedule.merge(data)
+
+    await schedule.save()
+
+    return schedule
   }
 
-  
+
   async destroy ({ params, request, response }) {
+    const schedule = await Schedule.findOrFail(params.id)
+
+    await schedule.delete()
   }
 }
 
